dd60394b0bf0a20cd6b2405d4528d5da4ac4b3be
Tauhid Rana <tauhidrana00@gmail.com>
2026-07-15 20:51:11 +0600

    STARTERS 247 problem solved

---
+++ Alternating_Array.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <functional>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <complex>
+#include <valarray>
+
+using namespace std;
+using ll = long long;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            if ((i % 2) != (x % 2))
+                cnt1++;
+            else
+                cnt2++;
+        }
+        int ans = min(cnt1, cnt2);
+        cout << ans << endl;
+    }
+
+    return 0;
+}
+
+/*
+   Coded by : KAZI TAUHID RANA (কাজী তৌহিদ রানা)
+     Codeforces Handle: tauhidrana.exe
+   Codechef username: tauhid_rana
+*/
---
+++ Substring_Shift.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <functional>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <complex>
+#include <valarray>
+
+using namespace std;
+using ll = long long;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    
+
+
+
+
+
+
+
+    return 0;
+}
+
+/*
+   Coded by : KAZI TAUHID RANA (কাজী তৌহিদ রানা)
+     Codeforces Handle: tauhidrana.exe
+   Codechef username: tauhid_rana
+*/
